e18170dd381f05b6b061623b1ae42f287e8c4e7a
Kevin Rexach <kaiorex@hotmail.com>
2024-09-18 12:11:21 -0400

    First part of Interactive I/O and Mathematical Expressions project.

--- CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
+++ CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
@@ -2,9 +2,26 @@
 //
 
 #include <iostream>
+#include <iomanip>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    double cups_sugar_original_recipe = 1.5, cups_butter_original_recipe = 1,
+        cups_flour_original_recipe = 2.75, cookies_in_recipe = 48, cookies_requested;
+    
+    cout << "Enter the number of cookies you would like to make: ";
+    cin >> cookies_requested;
+
+    double recipe_multiplier = cookies_requested / cookies_in_recipe;
+    double cups_sugar_new_recipe = cups_sugar_original_recipe * recipe_multiplier;
+    double cups_butter_new_recipe = cups_butter_original_recipe * recipe_multiplier;
+    double cups_flour_new_recipe = cups_flour_original_recipe * recipe_multiplier;
+
+    cout << setprecision(2) << fixed;
+    cout << "Cups of sugar needed: " << cups_sugar_new_recipe << endl;
+    cout << "Cups of butter needed: " << cups_butter_new_recipe << endl;
+    cout << "Cups of flour needed: " << cups_flour_new_recipe << endl;
+    return 0;
 }
 
