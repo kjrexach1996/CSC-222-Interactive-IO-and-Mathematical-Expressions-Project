e0c9e83dbf12c7be4b93aa10cba3ea3f93a46fe0
Kevin Rexach <kaiorex@hotmail.com>
2024-09-18 11:44:16 -0400

    First part of Interactive I/O and Mathematical Expressions project.

--- CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
+++ CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
@@ -22,12 +22,12 @@ int main()
     double interest = amount_in_savings - principal;
 
     cout << setprecision(2) << fixed;
-    cout << setw(20) << left << "Interest Rate:" << right << rate << "%" << endl;
-    cout << setw(20) << left << "Times Compounded:" << right << setprecision(0) << fixed << t << endl;
+    cout << setw(20) << left << "Interest Rate:" << setw(12) << right << rate << "%" << endl;
+    cout << setw(20) << left << "Times Compounded:" << setw(12) << right << setprecision(0) << fixed << t << endl;
     cout << setprecision(2) << fixed;
-    cout << setw(20) << left << "Principal:" << "$" << right << principal << endl;
-    cout << setw(20) << left << "Interest:" << "$" << right << interest << endl;
-    cout << setw(20) << left << "Amount in Savings:" << "$" << right << amount_in_savings << endl;
+    cout << setw(20) << left << "Principal:" << "$ " << setw(10) << right << principal << endl;
+    cout << setw(20) << left << "Interest:" << "$ " << setw(10) << right << interest << endl;
+    cout << setw(20) << left << "Amount in Savings:" << "$ " << setw(10) << right << amount_in_savings << endl;
     return 0;
 }
 
