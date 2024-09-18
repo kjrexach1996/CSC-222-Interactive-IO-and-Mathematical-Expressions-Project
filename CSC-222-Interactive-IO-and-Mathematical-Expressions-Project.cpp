b92891197c4d1739bbfec60066abf0e470886ce7
Kevin Rexach <kaiorex@hotmail.com>
2024-09-18 10:02:31 -0400

    Add project files.

---
+++ CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
@@ -0,0 +1,10 @@
+// CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
