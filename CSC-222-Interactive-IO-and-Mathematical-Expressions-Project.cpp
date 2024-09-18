5e836313aeb6972b767dc36f318b3db0899ff5cc
Kevin Rexach <kaiorex@hotmail.com>
2024-09-18 13:34:16 -0400

    First part of Interactive I/O and Mathematical Expressions project with comments.

--- CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
+++ CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
@@ -1,5 +1,6 @@
-// CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//This program takes a number of cookies inputted by the user and uses that number
+//to adjust the number of cups of each ingredient needed to produce that number of cookies
+//in the recipe
 
 #include <iostream>
 #include <iomanip>
@@ -7,17 +8,21 @@ using namespace std;
 
 int main()
 {
+    //Initialize variables
     double cups_sugar_original_recipe = 1.5, cups_butter_original_recipe = 1,
         cups_flour_original_recipe = 2.75, cookies_in_recipe = 48, cookies_requested;
     
+    //Input number of cookies requested
     cout << "Enter the number of cookies you would like to make: ";
     cin >> cookies_requested;
 
+    //Perform calculations and receive adjusted amounts of each ingredient in recipe
     double recipe_multiplier = cookies_requested / cookies_in_recipe;
     double cups_sugar_new_recipe = cups_sugar_original_recipe * recipe_multiplier;
     double cups_butter_new_recipe = cups_butter_original_recipe * recipe_multiplier;
     double cups_flour_new_recipe = cups_flour_original_recipe * recipe_multiplier;
 
+    //Display new amounts of each ingredient needed
     cout << setprecision(2) << fixed;
     cout << "Cups of sugar needed: " << cups_sugar_new_recipe << endl;
     cout << "Cups of butter needed: " << cups_butter_new_recipe << endl;
