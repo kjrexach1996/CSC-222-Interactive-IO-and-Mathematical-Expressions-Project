fb949d885c138f6089cf03b63cdb08c434c2b580
Kevin Rexach <kaiorex@hotmail.com>
2024-09-18 13:31:28 -0400

    Second part of Interactive I/O and Mathematical Expressions project with comments.

--- CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
+++ CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp
@@ -1,5 +1,6 @@
-// CSC-222-Interactive-IO-and-Mathematical-Expressions-Project.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//This program takes information by the user--principal, an interest rate, and a number
+//of times the interest compounds--to calculate the total amount in a savings account 
+//and the amount of accumulated interest.
 
 #include <iostream>
 #include <iomanip>
@@ -8,8 +9,10 @@ using namespace std;
 
 int main()
 {
+    //Initialize variables
     double rate, principal, t;
 
+    //Take user input
     cout << "Enter the principal in the savings account (in $0.00 format): ";
     cin >> principal;
     cout << "Enter the interest rate: ";
@@ -17,10 +20,12 @@ int main()
     cout << "Enter the number of times interest is compounded in a year: ";
     cin >> t;
 
+    //Calculate amount in savings account and accumulated interest
     double rate_decimal = rate / 100;
     double amount_in_savings = principal * pow((1 + (rate_decimal / t)), t);
     double interest = amount_in_savings - principal;
 
+    //Display all results in an organized list
     cout << setprecision(2) << fixed;
     cout << setw(20) << left << "Interest Rate:" << setw(12) << right << rate << "%" << endl;
     cout << setw(20) << left << "Times Compounded:" << setw(12) << right << setprecision(0) << fixed << t << endl;
